910409d7370cd4d79585557839a667419dc343db
arunmoy-das25 <30001326@sbstudents.org>
2024-11-21 12:41:58 -0500

    Explanation
    Added the explanation for multithreaded approach is slower at times.

--- Comparison.java
+++ Comparison.java
@@ -8,9 +8,18 @@ public class Comparison {
 
         int comparison = (int) (singleThread.getElapsedTime() - multiThread.getElapsedTime());
 
+        System.out.println("Single Thread Time: " + singleThread.getElapsedTime());
+        System.out.println("Multi Thread Time: " + multiThread.getElapsedTime());
+
         if(comparison > 0){
             System.out.println("The mutli threaded approach is faster by " + comparison + " ns");
         }
         else System.out.println("The single threaded approach is faster by " + Math.abs(comparison) + " ns");
     }
+
+    /*Why does the multithreaded approach take longer at time?
+        Creating each thread takes extra time for a multithreaded approach.
+        Meanwhile, the single threaded approach only needs to make one thread.
+        Hence, the more threads in the multithreaded approach, the slower it is in comparison to a single thread.
+    */
 }
